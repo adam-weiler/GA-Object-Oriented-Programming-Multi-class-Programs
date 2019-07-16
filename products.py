7a958758f2215a38cd16ee3f1b8e25cab3422a9b
adam-weiler <adam.weiler@yahoo.ca>
2019-07-16 15:23:57 -0400

    Creating products and shopping_cart files

---
+++ products.py
@@ -0,0 +1,20 @@
+class Product():
+    def __init__(self, name, base_price, tax_rate):
+        self.name = name
+        self.base_price = base_price
+        self.tax_rate = tax_rate
+
+    def total_price(self):
+        return self.base_price + (self.base_price * self.tax_rate)
+
+# computer = Product('Computer', 1500, .15)
+# print(computer.name)
+
+# television = Product('Television', 2500, .20)
+# print(television.name)
+
+# oculus_rift = Product('Oculus Rift', 999, .14)
+# print(oculus_rift.name)
+
+# iPhone = Product('iPhone', 800, .13)
+# print(iPhone.name)
---
+++ shopping_cart.py
@@ -0,0 +1,20 @@
+from products import Product
+
+class Shopping_Cart():
+    def __init__(self, product):
+        self.product = product
+
+    def add_to_cart()
+
+    
+
+computer = Product('Computer', 1500, .15)
+print(computer.name)
+
+
+#Shopping cart
+#Add a product to cart
+#Remove a product from cart
+#Add total cost of all products in cart (before tax)
+#Add total cost of all products in cart (after tax)
+
